ccb3b9a9db2c756b714966458191288be6acb95e
birendra20 <birendragupta20@gmail.com>
2022-09-20 13:20:52 +0530

    nested-routes

--- my-first-nextjs-app/next.config.js
+++ my-first-nextjs-app/next.config.js
@@ -2,6 +2,19 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
+// we then have the next.config.js file
+// which is the next.js configuration file
+// we just have react strict mode set to
+// true
+// react's strict mode is a development
+// mode only feature
+// for highlighting potential problems in
+// an application
+// it helps to identify unsafe life cycles
+// legacy api usage and a number of other
+// features
+// so i recommend you leave this set to
+// true
